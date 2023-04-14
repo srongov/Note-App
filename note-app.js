d57e1ab6ee11c285b65152d7341465c24ab73424
Hak Gov <srongov@yahoo.com>
2023-04-14 17:28:01 -0400

    adding to screen

--- note-app.js
+++ note-app.js
@@ -1,17 +1,23 @@
 let todos = []
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    body: ''
 }
 
 const renderTodos = function (todos, filters) {
     const filtersTodos = todos.filter(function (todo) {
-        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const filterSearchText = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const filterBody = todo.text.toLowerCase().includes(filters.body.toLowerCase())
+        return filterSearchText && filterBody
     })
     filtersTodos.forEach(function (todo) {
         const p = document.createElement('p')
         p.textContent = todo.text
         document.querySelector('body').appendChild(p)
+        const pEl = document.createElement('p')
+        pEl.textContent = todo.body
+        document.querySelector('body').appendChild(pEl)
     })
 
 }
@@ -22,8 +28,10 @@ renderTodos(todos, filters)
 document.querySelector('#new-todo').addEventListener('submit', function (e) {
     e.preventDefault()
     todos.push({
-        text: e.target.elements.text.value
+        text: e.target.elements.text.value,
+        body: e.target.elements.body.value
     })
     renderTodos(todos, filters)
-
+    e.target.elements.text.value = ''
+    e.target.elements.body.value = ''
 })
